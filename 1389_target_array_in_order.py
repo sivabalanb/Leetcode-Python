a0d2bc067b538c19e8f857aa2715a67cbfbb98af
sivabalan balasubramanian <sivabalanb.92@gmail.com>
2022-05-08 11:22:54 +0100

    target array in a order

---
+++ 1389_target_array_in_order.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        result = []
+        for i,j in zip(nums, index):
+            result.insert(j, i)
+        return result
