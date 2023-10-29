fe5ae53fe5d0dd635a9fb4753b78a9600e83cf7a
Sivabalan <sivabalanb.92@gmail.com>
2023-10-29 12:21:03 +0000

    Two sum solution - updated

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,10 +1,23 @@
-#Two Sum - https://leetcode.com/problems/two-sum/
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#https://leetcode.com/problems/two-sum/
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Creating an dict to store hash map
         hash_map = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
+
+        # Enumerate through the given array to form the hash_map
+        for i, num in enumerate(nums):
+
+            # Calculate complement
+            complement = target - num
+
+            # Check if complement is already in hash
             if complement in hash_map:
                 return (hash_map[complement], i)
-            else:
-                hash_map[nums[i]] = i
+            
+            hash_map[num] = i
+
