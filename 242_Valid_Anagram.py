f9f7870691d38066addd66bd13fcc7a968ff9aa5
Sivabalan <sivabalanb.92@gmail.com>
2023-11-01 09:42:52 +0000

    Valid anagram

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/valid-anagram/
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        from collections import Counter
+        return Counter(s) == Counter(t)
